cd256574ace8dcb7f0ac704f3f25d6fabd65b381
Zer02 <abasdeo29@gmail.com>
2021-06-13 21:20:59 -0400

    initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import firebase from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDiTV6kcCrV_hxXvCOtR8R0QrUTDwhzGzY",
+  authDomain: "udemy-ninja-chat-3a662.firebaseapp.com",
+  databaseURL: "https://udemy-ninja-chat-3a662.firebaseio.com",
+  projectId: "udemy-ninja-chat-3a662",
+  storageBucket: "udemy-ninja-chat-3a662.appspot.com",
+  messagingSenderId: "587447912996",
+  appId: "1:587447912996:web:e12e9a2b644e23e4cb26b1"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, timestamp }
