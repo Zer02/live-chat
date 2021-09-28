9c3e0d14ac61a41468ea0a5475dfb0775a83958f
Anil <abasdeo29@gmail.com>
2021-09-27 22:58:39 -0400

    Completed live-chat project

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="container">
     <Navbar />
+    <ChatWindow />
     <NewChatForm />
   </div>
 </template>
 
 <script>
 import NewChatForm from '../components/NewChatForm'
+import ChatWindow from '../components/ChatWindow'
 import Navbar from '../components/Navbar'
 import getUser from '../composables/getUser'
 import { watch } from '@vue/runtime-core'
 import { useRouter } from 'vue-router'
 
 export default {
-  components: { Navbar, NewChatForm },
+  components: { Navbar, NewChatForm, ChatWindow },
   setup() {
     const { user } = getUser()
     const router = useRouter()
